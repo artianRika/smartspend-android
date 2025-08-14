97c63e36871f67bf4b69b47bbb37dd05c5d4ca68
Artian RIKA <artianrika@gmail.com>
2025-08-14 16:01:02 +0200

    fix: balance card

--- app/src/main/java/com/phantom/smartspend/ui/components/BalanceCard.kt
+++ app/src/main/java/com/phantom/smartspend/ui/components/BalanceCard.kt
@@ -1,13 +1,20 @@
 package com.phantom.smartspend.ui.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.CallMade
-import androidx.compose.material.icons.outlined.CallReceived
-import androidx.compose.material.icons.outlined.QrCodeScanner
+import androidx.compose.material.icons.outlined.Paid
+import androidx.compose.material.icons.outlined.Receipt
 import androidx.compose.material.icons.outlined.RemoveRedEye
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,7 +28,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun BalanceCard() {
@@ -30,8 +41,8 @@ fun BalanceCard() {
     var currency by remember { mutableStateOf("MKD") }
 
 
-    Column(Modifier.fillMaxWidth()) {
-        Text("Your Balance", color = MaterialTheme.colorScheme.primary)
+    Column(Modifier.fillMaxWidth(),
+        Arrangement.spacedBy(4.dp)) {
 
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -39,48 +50,74 @@ fun BalanceCard() {
             modifier = Modifier.fillMaxWidth()
 
 
-
-
         ) {
-            Text("$amount $currency")
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Outlined.RemoveRedEye,
-                    contentDescription = "Show",
-                    tint = MaterialTheme.colorScheme.primary
-                )
+            Column(Modifier.fillMaxWidth()) {
+                Text("Your Balance", color = MaterialTheme.colorScheme.primary, fontSize = 12.sp)
+                Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
+                    Text("$amount $currency", fontSize = 28.sp)
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Outlined.RemoveRedEye,
+                            contentDescription = "Show",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             }
         }
         Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            Arrangement.SpaceBetween
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Outlined.CallMade,
-                    contentDescription = "Expense",
-                    tint = MaterialTheme.colorScheme.primary
-                )
+            Row(
+                Modifier
+                    .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(12.dp)
+                ).padding(vertical = 8.dp, horizontal = 18.dp),
+                Arrangement.spacedBy(50.dp)
+            ) {
+                IconWithTextButton(
+                    "Savings",
+                    Icons.Outlined.Paid
+                ) { }
+
+                IconWithTextButton(
+                    "Transactions",
+                    Icons.Outlined.Receipt
+                ) {}
             }
 
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Outlined.CallReceived,
-                    contentDescription = "Income",
-                    tint = MaterialTheme.colorScheme.primary
-                )
-            }
-
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Outlined.QrCodeScanner,
-                    contentDescription = "Scan",
-                    tint = MaterialTheme.colorScheme.primary
-                )
-            }
         }
 
+    }
+}
+
+@Composable
+fun IconWithTextButton(
+    text: String,
+    icon: ImageVector,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp))
+            .clickable(onClick = onClick)
+            .padding(8.dp)
+            .width(80.dp).height(50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = text,
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text, fontSize = 12.sp)
     }
 }
 
